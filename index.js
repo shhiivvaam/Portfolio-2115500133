759a8e980b5b22788f42a498ba804bfc7725e2a8
Shivam Kumar <96204332+shhiivvaam@users.noreply.github.com>
2023-04-16 23:18:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// Auto Type Code
+
+const { default: Typed } = require("typed.js");
+
+var typed;
+
+typed = new Typed(".auto-type", {
+    strings: [" Hello, Shivam this side! 😎", "I'm happy to see you here ✌️😍😍", "Will be good if we Connect" , "Have a Good day! 🤞😎"],
+    typeSpeed: 200,
+    backSpeed: 200,
+    loop : true
+});
